efea5ba77e183351d83eb67e91c41a3d1a0bd2be
aliberman@ucdavis.edu <aliberman@ucdavis.edu@9c626006-3ddf-1dcb-ec0c-b68a781fdc54>
2014-05-22 14:43:20 +0000

    -F

--- trunk/pfam-hmm-parser-v01.py
+++ trunk/pfam-hmm-parser-v01.py
@@ -47,7 +47,6 @@ def get_space_array(line):
 		
 		if in_word:
 			arr[len(arr) - 1] = arr[len(arr) - 1] + c
-	
 	return arr
 
 #tab delimiter
@@ -59,6 +58,7 @@ def tab_separate_from_array(arr):
 	result = result[:-1] #chop off the extra tab
 	return result
 #find line of id and append
+
 def seq_get_id(fin, identifier, count):
 	f = open(fin, 'rb')
 
@@ -75,28 +75,30 @@ def seq_get_id(fin, identifier, count):
 			line = re.sub('\[\]','C',line)
 			line = re.sub('\.\]','R',line)
 			line = re.sub('\[\.','F',line)
+			
 			arr.append(line)
 			count = count - 1
 			if count == 0:
 				break
-
 	f.close()
 	return arr
 #calls id and seq
-def seq_get_all_ids(fin, lines):
+def seq_get_all_ids(fin, lines, opt):
 	result = ''
 	offset = 0
 	if opts and 'g' in opts:
-		offset = 2
+		offset = 3
 	for line in lines:
 		identifier = line[8 + offset]
 		count = int(line[7 + offset])
-		result = result + identifier + '\n'
 		seqs = seq_get_id(fin, identifier, count)
+		
 		for seq in seqs:
-			result = result + seq
-		result = result + '\n'
+			result = result + identifier + seq
 	return result
+
+
+
 
 #second tabber for second ar7ray(evalues)
 def tab_separated_evals(evalues):
@@ -115,14 +117,12 @@ def eval_get_array (ecount, fin, fout, opt):
 		if check:
 			pos= line.find('E-value  score  bias    E-value  score  bias    exp  N  Sequence   ')
 			if opt and 'g' in opt:
-				line = 'Query	Accession' + line
+				line = 'Query	Accession	Description' + line
 			if pos != -1:
 				check = False
-				#option code
 				if ecount != 0:	
 					if opt and 'h' in opt:
 						to_print = tab_separate_from_array(get_space_array(line))
-						print to_print
 						EVal_FILE.write(to_print)
 					next(hmm_FILE)
 		else:
@@ -139,26 +139,47 @@ def eval_get_array (ecount, fin, fout, opt):
 	return lines
 
 def get_quar_assent(fin):
-	hmm_FILE  = open(fin, "rb")
+	hmm_FILE  = open(fin, "r")
+	q_str = ''
+	a_str = ''
+	d_str = ''
+	
 	for line in hmm_FILE:
-		
-		query = re.match('Query:[\s\t]+(\w+)[\s\t]+\[(\w+)\=\d+\]',line)
+		query = re.match('Query:[\s\t]+(\S+)[\s\t]+\[(\w+)\=\d+\]',line)
 		if query:
-			return query.group(1), query.group(2)	
-		accen = re.match('Accession:[\s\t]+(\w+\d+\.\d+)',line)
-		if accen:
-			print accen.group(0) + 'wrherhethejhseerbhiehrieorhj'
-		else:
-			print 'cant find'
+			q_str= query.group(1)
+			
+			
+		acces = re.match("Accession:[\s\t]+(\S+)[\s\t\n]+",line)
+		if acces:
+			a_str = acces.group(1)
+		desc = re.match("Description:[\s\t]+(.*)",line)
+		if desc:
+			d_str = desc.group(1)
+	
+	
+	return q_str, a_str, d_str	
 
-# script options #
+def fout2_headers(fout2, opt):
+	f2  = open(fout2, "a")
+	to_print = ''
+	new_print = []
+	check = True
+	hmm_FILE  = open(fin, "rb")
+	EVal2  = open(fout2, "a")
+	to_print = 'Sequence #  type  score  bias  c-Evalue  i-Evalue hmmfrom  hmm to    alifrom  alito type   envfrom  envto  type   acc'
+	new_print[:] = to_print.split() 
+	new_print = tab_separate_from_array(new_print) + '\n'
+	f2.write(new_print)
+
+
 if __name__ == '__main__':
 	if len(sys.argv) < 4 or len(sys.argv) > 5:
 		print len(sys.argv[:])
 		print "[1] = file_input [2]=evalue_output [3]=id_details_output"
 		print "if [4]=y append header: 'E-value  score  bias....' to evalue_output to evalue_output" 
-		print "g gives accession and query to file 1"
-		print "h = prints query acession headers"
+		print "g = add 3 columbs for accession, query, and description for file 1"
+		print "h = prints headers for both filesS"
 		sys.exit(1)
 
 	fin = sys.argv[1]
@@ -167,29 +188,33 @@ if __name__ == '__main__':
 	#option to print E-Val header line' E-value  score  bias....Description' in file 1
 	if len(sys.argv) == 5:
 		opt = sys.argv[4]
-			#do other tuff
 	else:
 		opt = 0
 	opts = opt
 	
 	hits = domain_HITS(fin)
-
-	evalues = eval_get_array(hits, fin, fout, opt)
 	
-	zip, acc = get_quar_assent(fin)
+	evalues = eval_get_array(hits, fin, fout, opt)
+	q_str, a_str, d_str = get_quar_assent(fin)
 	if opt and 'g' in opt:
 		for evalue in evalues:
-			evalue.insert(0, acc)	
-			evalue.insert(0, zip)
-
+			evalue.insert(0, d_str)
+			evalue.insert(0, a_str)	
+			evalue.insert(0, q_str)
+	
+	if opt and 'h' in opt:
+		fout2_headers(fout2, opt)
 	evals_string = tab_separated_evals(evalues)
-	id_string = seq_get_all_ids(fin, evalues)
+	id_string = seq_get_all_ids(fin, evalues, opt)
+
+	
 	f = open(fout, 'a')
 	f2 = open(fout2, 'a')
 	f.write(evals_string)
-	print evals_string
+	# print evals_string
 	f.write('\n')
 	f2.write(id_string)
+	# print id_string
 	f.close()
 	f2.close()
 
